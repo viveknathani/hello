9967a57643deb79892b7d1fc58331bb0f7783fe9
viveknathani <viveknathani2402@gmail.com>
2021-07-13 21:56:51 +0530

    chore: load environment variables
    Parsing the .env file requires using godotenv package.

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func init() {
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Could not load .env file!")
+	}
+}
 
 func main() {
+
 	fmt.Println("Hello!")
 }
